c47789b7ef08edccac78f8f19141ac7590b7e03b
Stinger <maxym.sobol@gmail.com>
2022-09-02 11:32:30 +0300

    Add Javadoc comments about packages.

---
+++ src/main/java/com/labs/two/transport/package-info.java
@@ -0,0 +1,6 @@
+package com.labs.two.transport;
+
+/**
+ * Package which has all classes of some kinds of transport
+ * Now there is basic class Transport and Train
+ */
---
+++ src/main/java/com/labs/two/util/package-info.java
@@ -0,0 +1,7 @@
+package com.labs.two.util;
+
+/**
+ * Contains utility classes
+ * Interface printable
+ * TrainSchedule - to work with array of trains
+ */
